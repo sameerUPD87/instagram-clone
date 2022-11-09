ffbcd111ce79f572755c3b83e303ec3f86eb2fb2
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2022-11-09 19:42:38 +0530

    created user-auth-listener and user context

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React, { lazy, Suspense } from "react";
 import * as ROUTES from "./constants/routes";
+import UserContext from "./context/user";
+import useAuthListener from "./hooks/use-auth-listener";
 
 const Login = lazy(() => import("./pages/login"));
 const SignUp = lazy(() => import("./pages/sign-up"));
+const Dashboard = lazy(() => import("./pages/dashboard"));
 const NotFound = lazy(() => import("./pages/not-found"));
 
 function App() {
-  return (
-    <Router>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Routes>
-          {/* <Route path="/login" component={<Login />} />  */}
-          {/* this is not used after react router dom v6 */}
+  const { user } = useAuthListener();
 
-          <Route path={ROUTES.LOGIN} element={<Login />} />
-          <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
-          <Route element={<NotFound />} />
-        </Routes>
-      </Suspense>
-    </Router>
+  return (
+    <UserContext.Provider value={{ user }}>
+      <Router>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Routes>
+            {/* <Route path="/login" component={<Login />} />  */}
+            {/* this is not used after react router dom v6 */}
+
+            <Route path={ROUTES.LOGIN} element={<Login />} />
+            <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
+            {/* <ProtectedRoute user={user} path={ROUTES.DASHBOARD} exact>
+            <Dashboard />
+          </ProtectedRoute> */}
+            <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
+            <Route element={<NotFound />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
