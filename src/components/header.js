c80c0bf544d64718980f3e9557d4e2c082a12f2e
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2022-11-09 21:02:12 +0530

    added use-user hook, logged-in-user context and images path

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
 import UserContext from "../context/user";
 import * as ROUTES from "../constants/routes";
@@ -10,7 +10,7 @@ export default function Header() {
   const { user: loggedInUser } = useContext(UserContext);
   const { user } = useUser(loggedInUser?.uid);
   const { firebase } = useContext(FirebaseContext);
-  const history = useHistory();
+  const history = useNavigate();
 
   return (
     <header className="h-16 bg-white border-b border-gray-primary mb-8">
